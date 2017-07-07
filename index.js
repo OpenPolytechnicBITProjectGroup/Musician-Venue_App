a79fd67c9bf9ad1bfbd4ea2a0cd2b8cda5cd4cbc
Jack Scott <123.jack.scott@gmail.com>
2017-07-07 22:19:39 +1200

    Fixed node port

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const app = express()
 app.get('/', function (req, res) {
   res.send('<h1>Yay it works!</h1><p>Express.js Example App</p>')
 })
+app.set('port', (process.env.PORT || 5000));
 
-app.listen(3000, function () {
+app.listen((app.get('port'), function () {
   console.log('We have lift-off!!')
 })
