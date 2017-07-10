e3cbd15eded952b35a81f3890436767044cfb153
Alex McBride <lxdnz34@gmail.com>
2017-07-10 22:17:57 +1200

    Changed api driver to neo4j-driver. getAllVenues returns a NodeVenue … (#8)
    * Changed api driver to neo4j-driver. getAllVenues returns a NodeVenue object which is the same as a Venue object.
    
    * Update db_api.js
    
    Added BOLT_ to process.env variables..

--- db_api/db_api.js
+++ db_api/db_api.js
@@ -1,75 +1,96 @@
-var neo4j = require('neo4j');
+var neo4j = require('neo4j-driver').v1;
 var _ = require('lodash');
 var Venue = require('./venue.js');
 
-var db = new neo4j.GraphDatabase({
-    // Support specifiying database info via enviroment variables
-    // but assume Neo4j installation defaults
-    url: process.env['NEO4J_URL'] || process.env['GRAPHENEDB_URL'] ||
-        'http://neo4j:neo4j2@localhost:7474',
-    auth: process.env['NEO4J_AUTH'],
-});
+var url = (process.env['NEO4J_URL'] || process.env['GRAPHENEDB_URL'] ||
+        'http://neo4j:neo4j2@localhost:7474')
+var bolt_url = (process.env['NEO4J_BOLT_URL'] || process.env['GRAPHENEDB_BOLT_URL'] ||
+        'bolt://localhost');
+var bolt_user = (process.env['NEO4J_BOLT_USER'] || process.env['GRAPHENEDB_BOLT_USER'] ||
+        'neo4j');
+var bolt_pass = (process.env['NEO4J_BOLT_PASSWORD'] || process.env['GRAPHENEDB_BOLT_PASSWORD'] ||
+        'neo4j2');
+
+
+var db = neo4j.driver(bolt_url, neo4j.auth.basic(bolt_user, bolt_pass));
 
 // Tester function to make sure DB is going
 function test() {
-    db.cypher({
-        query: 'MERGE (n:Test {name: {testName}}) RETURN n',
-        params: {
-            testName: 'Bob'
-        }
-    }, function(err, results){
-        var result = results[0];
-        if (err) {
-            console.error('Error saving new node to database:', err);
-        } else {
-            console.log('Node saved to database with id:', result['n']['_id']);
-        }
-    }); 
+    var session = db.session();
+    
+    const resultPromise = session.writeTransaction(tx => tx.run(
+        'MERGE (n:Test {name: {testName}}) RETURN n ',
+        {testName: 'Bob'}));
+
+    resultPromise.then(result => {
+    session.close();
+
+    const singleRecord = result.records[0];
+    const greeting = singleRecord.get(0);
+
+    console.log(greeting);
+
+    }).catch(error => {
+        console.log("In Test: ", error);
+    });
+
     venue = new Venue.Venue("The Grand", 500, "Wellington", ["Cheese", "Slapper"]);
     createVenue(venue);
-    getAllVenues();
-
+ 
+    getAllVenues().then(venues => {
+            if (venues) {
+                venues.forEach(venue =>{
+                    console.log("In Test: " ,venue);
+                });
+            }
+        });
 }
 
 // Send a Venue object to here to create Venue in the database
 function createVenue(venue) {
-    db.cypher({
-        query: "MERGE (v:Venue {name: {name}, capacity: {capacity}, \
+    var session = db.session();
+    
+    const resultPromise = session.writeTransaction(tx => tx.run(
+        "MERGE (v:Venue {name: {name}, capacity: {capacity}, \
                 location: {location}, genres: {genres}}) RETURN v",
-        params: {
-            name: venue.name,
-            capacity: venue.capacity,
-            location: venue.location,
-            genres: venue.genres
-        }
-    }, function(err, results){
-        var result = results[0];
-        if (err) {
-            console.error('Error saving new venue to database:', err);
-        } else {
-            console.log('Successfully saved new venue with id:', result['v']['_id']);
-        }
+        {name: venue.name,
+        capacity: venue.capacity,
+        location: venue.location,
+        genres: venue.genres}));
+
+    resultPromise.then(result => {
+    session.close();
+
+    const singleRecord = result.records[0];
+    const oneVenue = singleRecord.get(0);
+
+    console.log(oneVenue);
+
+    }).catch(error => {
+        console.log(error);
     });
 }
 
 // Returns all the venues in the database
 function getAllVenues() {
-    db.cypher({
-        query: 'MATCH (v:Venue) RETURN v'
-    }, function (err, request) {
-        var result = request[0];
-        if (err) {
-            console.error('Error saving new venue to database:', err);
-        } else {
-            if (_.isEmpty(result)) {
-                console.log("Empty search result");
-                return null;
-            }
-            console.log('Success getting matches:', result['v']['_id']);
-            return request;
-        }
+   
+    var session = db.session();
+    
+    const resultPromise = session.readTransaction(tx => tx.run(
+        'MATCH (v:Venue) RETURN v'
+        ));
+
+    return resultPromise.then(result => {
+    session.close();
+
+    return result.records.map(record => {
+        return new Venue.NodeVenue(record.get('v'));
+        });
+    }).catch(error => {
+        console.log(error);
     });
 }
+
 
 function createBand(band) {
 
--- db_api/venue.js
+++ db_api/venue.js
@@ -1,4 +1,6 @@
-// Constructor for Venue objects.
+var _ = require('lodash')
+
+// Constructor for Venue objects before the 
 function Venue(name, capacity, location, genres) {
 		this.name = name;
 		this.capacity = capacity;
@@ -6,4 +8,13 @@ function Venue(name, capacity, location, genres) {
 		this.genres = genres;
 }
 
+// Constructor for Venue objects from the database.
+function NodeVenue(_node) {
+	_.extend(this, _node.properties)
+		
+		if (this.capacity)
+		this.capacity = this.capacity.toNumber();
+}
+
 exports.Venue = Venue;
+exports.NodeVenue = NodeVenue;
