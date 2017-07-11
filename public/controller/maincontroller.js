9cd2710dc752c6b36938488f405255b4d87a568a
lxdnz254 <lxdnz34@gmail.com>
2017-07-11 21:42:44 +1200

    Added example api call from client >> server >> api >> server >> client. This process will work for any api call.

--- public/controller/maincontroller.js
+++ public/controller/maincontroller.js
@@ -1,6 +1,6 @@
 var app = angular.module('tester', []);
 
-app.factory("Test", function() {
+app.factory("Test", [('$scope', '$http'), function() {
   var bands = ["Six60", "Shihad", "Exponents"];
   var working = "A description of the band";
 
@@ -11,8 +11,14 @@ app.factory("Test", function() {
       working: function () {
         return working;
       }
+      
     }
-})
+}]);
+
+function postVenues($scope, venues) {
+  console.log("postingVenues:", venues);
+  $scope.other_venues = venues;
+}
 
 // Returns some test Venue objects. 
 function addTestVenues() {
@@ -20,7 +26,6 @@ function addTestVenues() {
   
 	venues.push(new Venue("Crown Hotel", 500, "Auckland", ["Rock", "Blues", "Country"]));
 	venues.push(new Venue("Bar Sinister", 300, "Christchurch", ["Metal", "Rock"]));
-
 	return venues;
 }
 
@@ -32,4 +37,17 @@ app.controller('MainCtrl', function ($scope, Test) {
 app.controller('WorkingCtrl', function ($scope, Test) {
   $scope.working = Test.working();
 });        
-	
+
+// this controller is hooked to second table $scope.other_venues	
+app.controller('VenueCtrl', function ($scope, $http) {
+  
+     // Send request to server
+        console.log("sending request to server");
+        $http.get('/other_venues')
+          .then(function (resp) {
+            // sends the returned data to the $scope item
+            $scope.other_venues = resp.data;
+            console.log("request back:", resp.data);         
+          });      
+});
+
