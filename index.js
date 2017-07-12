d4c42c390987b777017410593c5a96e76443b660
Jack Scott <123.jack.scott@gmail.com>
2017-07-12 18:21:00 +1200

    Removed static link to bootstrap folder as the link to ./public works.

--- index.js
+++ index.js
@@ -5,37 +5,36 @@ var Venue = require('./db_api/venue.js');
 var db = require('./db_api/db_api.js');
 
 app.use('/', express.static(__dirname + '/public'));
-app.use('/css/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 
 app.get('/working', function (req, res) {
-  console.log('Working recieved');
-  res.send('<h1>Yay it works!</h1><p>Express.js Example App</p>');
+    console.log('Working recieved');
+    res.send('<h1>Yay it works!</h1><p>Express.js Example App</p>');
 });
 
 // Gets request from client and activates api call
 app.get('/other_venues', function (req, res) {
-  
-  console.log("Received request from client");
-  var send = [];
-  db.getAllVenues().then(venues => {
-            if (venues) {
-                venues.forEach(venue =>{
-                    send.push(new Venue.Venue(
-                        venue.name, venue.capacity,
-                        venue.location, venue.genres
-                    ));
-                
-                });
-            }
-            res.send(send)
-        });
+
+    console.log("Received request from client");
+    var send = [];
+    db.getAllVenues().then(venues => {
+        if (venues) {
+            venues.forEach(venue => {
+                send.push(new Venue.Venue(
+                    venue.name, venue.capacity,
+                    venue.location, venue.genres
+                ));
+
+            });
+        }
+        res.send(send)
+    });
 });
 
 app.set('port', (process.env.PORT || 5000));
 
 app.listen(app.get('port'), function () {
-  console.log('We have lift-off!!');
-  // these tests will only work if neo4j is running (locally or on heroku)
-  // otherwise remove before running
-  db.test();
+    console.log('We have lift-off!!');
+    // these tests will only work if neo4j is running (locally or on heroku)
+    // otherwise remove before running
+    db.test();
 });
