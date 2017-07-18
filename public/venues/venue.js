aa793a65f3d088f9bb7c3f0bc9ad4d1b1827ded3
Jack Scott <123.jack.scott@gmail.com>
2017-07-18 20:41:04 +1200

    Deleted old app and public dirs
    Got carried over to this branch for some reason?

--- public/venues/venue.js
+++
@@ -1,7 +0,0 @@
-// Constructor for Venue objects.
-function Venue(name, capacity, location, genres) {
-		this.name = name;
-		this.capacity = capacity;
-		this.location = location;
-		this.genres = genres;
-	}
