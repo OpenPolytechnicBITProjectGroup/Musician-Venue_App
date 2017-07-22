179b1511e73d42e7a00dc3a3d6f3ad6d3f734fe1
Jack Scott <123.jack.scott@gmail.com>
2017-07-22 16:17:57 +1200

    Set up gulp to concatenate js files
    See README.md for more info

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+
 
 // Compile less to css
 gulp.task('less', function () {
@@ -11,9 +13,23 @@ gulp.task('less', function () {
         .pipe(gulp.dest('client/css'));
 });
 
+// Concatenate/package js files
+gulp.task('scripts', function () {
+    return gulp.src([
+        './node_modules/angular/angular.js',
+        './node_modules/jquery/dist/jquery.js',
+        './client/js/controllers/*.js',
+        './client/js/venues/*.js'
+    ])
+        .pipe(concat('package.js'))
+        .pipe(gulp.dest('./client/js/'));
+});
+
+
 // Watch scss folder for changes
 gulp.task('watch', function () {
     gulp.watch("assets/less/*", ['less']);
+    gulp.watch("client/js/*", ['scripts'])
 });
 
 gulp.task('default', ['less']);
