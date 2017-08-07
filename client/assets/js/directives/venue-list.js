bfbf6d660125941d3ad0efbf5761b7860377f765
lxdnz254 <lxdnz34@gmail.com>
2017-08-08 09:31:47 +1200

    Solved.. point directive templates to "/views/templates/*.html" . That was the missing piece of the puzzle.

--- client/assets/js/directives/venue-list.js
+++ client/assets/js/directives/venue-list.js
@@ -5,19 +5,7 @@ app.directive('venueList', function() {
 		scope: {
 			responseVenue: "=" 
 		},
-		//templateUrl: 'venue.html'
-		
-		template: 
-		'<div class="col-xs-12 col-md-6 col-lg-3">' +
-			'<div class="card">' +
-				'<h4>{{responseVenue.name}}</h4>' +
-				'<ul>' +
-					'<li>Location: {{responseVenue.location}}</li>' +
-					'<li>Capacity: {{responseVenue.capacity}}</li>' +
-					'<li>Preferred Genres: <span ng-repeat="genre in responseVenue.genres">{{genre}}{{$last ? "" : ", "}}</span></li>' +
-				'</ul>' +
-			'</div>' +
-		'</div>'
+		templateUrl: '/views/templates/venue.html'
 		
 	}
 });
