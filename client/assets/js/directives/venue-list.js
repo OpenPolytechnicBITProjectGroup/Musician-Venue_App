01d994dcd4ba0c8a8f4dab257189789d71f88133
lxdnz254 <lxdnz34@gmail.com>
2017-08-07 23:07:09 +1200

    Works when the html is written direct to directive

--- client/assets/js/directives/venue-list.js
+++ client/assets/js/directives/venue-list.js
@@ -1,11 +1,23 @@
 app.directive('venueList', function() {
 	return {
 		restrict: 'E',
-		templateUrl: 'venue.html',
 		replace: true,
 		scope: {
 			responseVenue: "=" 
-		}
+		},
+		//templateUrl: 'venue.html'
+		
+		template: 
+		'<div class="col-xs-12 col-md-6 col-lg-3">' +
+			'<div class="card">' +
+				'<h4>{{responseVenue.name}}</h4>' +
+				'<ul>' +
+					'<li>Location: {{responseVenue.location}}</li>' +
+					'<li>Capacity: {{responseVenue.capacity}}</li>' +
+					'<li>Preferred Genres: <span ng-repeat="genre in responseVenue.genres">{{genre}}{{$last ? "" : ", "}}</span></li>' +
+				'</ul>' +
+			'</div>' +
+		'</div>'
 		
 	}
 });
